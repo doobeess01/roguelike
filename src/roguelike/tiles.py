e8d92819ec9449fd9cb584175be74d77800e405c
David Dee <david.m.dee01@gmail.com>
2026-08-13 23:19:35 -0500

    Removed redundant TileGraphic class

--- src/roguelike/tiles.py
+++ src/roguelike/tiles.py
@@ -6,6 +6,9 @@ from typing import NamedTuple
 
 import numpy as np
 
+from .components import Graphic
+
+
 GRAPHIC_DTYPE = np.dtype(
     [
         ("ch", np.intc),
@@ -26,24 +29,19 @@ TILE_DTYPE = np.dtype(
 """Tile data type."""
 
 
-class TileGraphic(NamedTuple):
-    ch: int
-    fg: tuple[int, int, int] = (255, 255, 255)
-    bg: tuple[int, int, int] = (0, 0, 0)
-
 class NewTile(NamedTuple):
     """Helper class for new tiles."""
 
     name: str
-    graphic: TileGraphic
+    graphic: Graphic
     walkable: bool = False
     transparent: bool = False
 
 
 TILE_DATA = np.array(
     [
-        NewTile(name="wall", graphic=TileGraphic(ch=(ord("#")), fg=(255, 255, 255), bg=(0, 0, 0)), walkable=False, transparent=False),
-        NewTile(name="floor", graphic=TileGraphic(ch=(ord(".")), fg=(255, 255, 255), bg=(0, 0, 0)), walkable=True, transparent=True),
+        NewTile(name="wall", graphic=Graphic(ch=(ord("#")), fg=(150, 150, 150), bg=(0, 0, 0)), walkable=False, transparent=False),
+        NewTile(name="floor", graphic=Graphic(ch=(ord(".")), fg=(150, 150, 150), bg=(0, 0, 0)), walkable=True, transparent=True),
     ],
     dtype=TILE_DTYPE,
 )
