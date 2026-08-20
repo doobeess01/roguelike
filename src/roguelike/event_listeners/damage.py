c81a764a4f954e3f1cf1b88122203b8e7eb69522
David Dee <david.m.dee01@gmail.com>
2026-08-20 10:57:03 -0500

    Fixed message colors

--- src/roguelike/event_listeners/damage.py
+++ src/roguelike/event_listeners/damage.py
@@ -8,7 +8,7 @@ from .. import colors
 def apply_damage(damage: Damage):
     damage.target.components[HP] -= damage.amount
 
-    message_color = (colors.messages.PLAYER_ATTACKS_NPC if damage.source == g.player() else colors.messages.NPC_ATTACKS_PLAYER) if g.player in (damage.source, damage.target) else colors.messages.NPC_ATTACKS_NPC
+    message_color = (colors.messages.PLAYER_ATTACKS_NPC if damage.source == g.player() else colors.messages.NPC_ATTACKS_PLAYER) if g.player() in (damage.source, damage.target) else colors.messages.NPC_ATTACKS_NPC
     g.message_log.log(f"{get_name(damage.source)} attacks {get_name(damage.target)} for {damage.amount} damage!", *message_color)
 
     if damage.target.components[HP] <= 0:
